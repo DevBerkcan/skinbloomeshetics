ae7d2b2b8e5ddf25265d11997282eb8def533c52
DevBerkcan <atesoglu.berkcan@outlook.com>
2025-09-21 17:37:54 +0200

    add: google Tag

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,6 @@
+// app/layout.tsx
+"use client";
+
 import { Montserrat, Playfair_Display, Lato } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
@@ -5,10 +8,10 @@ import "./globals.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
-// Import Font Awesome
-import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the FontAwesome styles
+// Font Awesome
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-// Import local fonts
+// Local Fonts
 import localFont from "next/font/local";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,14 +24,14 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// Import Google Fonts
+// Google Fonts
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
-
-const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
+const playfairDisplay = Playfair_Display({ subsets: ["latin"], weight: ["400", "700"] });
 const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
+
+import Script from "next/script";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-52KB99G3"; // optional .env
 
 export const metadata = {
   title: "Skinbloom",
@@ -39,20 +42,46 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16908246874"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        {/* --- Google Tag Manager: so weit oben im <head> wie möglich --- */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
+        {/* --- Dein bestehendes gtag (Google Ads) – kann bleiben oder in GTM umziehen --- */}
+        <Script
+          id="gtag-src"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16908246874"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-16908246874');
-          `,
-        }} />
+          `}
+        </Script>
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.className} ${playfairDisplay.className} ${lato.className}`}
       >
+        {/* --- Google Tag Manager (noscript) – direkt unter <body> --- */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Header />
         {children}
         <Footer />
